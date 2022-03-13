6d95e6172e3e776c1381f8fecf3b7c4337e3e0fa
Anthony Khaiat <92814726+akhaiat2@users.noreply.github.com>
2022-03-12 23:28:53 -0600

    Progress on generating a Poem

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # poetrygenerator
-Program that generates poetry along with an image associated with the poem.
+Program that generates poetry along with an image associated with the poem. Inspired by Raymond Queneau's work Cent Mille Milliards de Poèmes with associated images.
+
+This program uses the Poemist API, Google Images API, DOM API alongside Javascript, CSS, and HTML.
--- js/main.js
+++ js/main.js
@@ -1,19 +1,21 @@
-  const canvas = document.querySelector('#mycanvas');
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-  const ctx = canvas.getContext('2d');
+  const poemDiv = document.querySelector('#poem');
 
   function createRandomPoem (poemText, i) {
     const text = new Text();
     const sentence = poemText.split('\n');
-    console.log(sentence[3]);
+    console.log(sentence);
+    // const poemSelectorNumber = Math.floor(Math.random()*5);
+    for (let i = 0; i <= sentence.length; i++) {
+      let poemContent = document.createTextNode(sentence);
+      poemDiv.appendChild(poemContent);
+    }
   }
 
 	async function getPoem () {
     const res = await window.fetch('https://www.poemist.com/api/v1/randompoems');
     const data = await res.json();
-    //console.log(data);
     data.forEach((o, i) => createRandomPoem(o.content, i));
   }
 
+//Google Image API surrounding poem? Tone.js for music
   getPoem();
